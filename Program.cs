0086e67024aba7a1e74d7c2921ae28f16d35e681
its julii <nanji828@gmail.com>
2023-01-22 11:58:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07.Working_Hours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = int.Parse(Console.ReadLine());
+            string dayOfWeek = Console.ReadLine();
+
+            if (dayOfWeek == "Sunday")
+            {
+                Console.WriteLine("closed");
+            }
+            else
+            {
+                if (time < 10 || time > 18)
+                {
+                    Console.WriteLine("closed");
+                }
+                else
+                {
+                    Console.WriteLine("open");
+                }
+            }
+        }
+    }
+}
